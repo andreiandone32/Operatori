2e2dc633a419d27ff9f21d5fd4f5a51b1c71827c
andreiandone32 <andoneandrei776@gmail.com>
2023-01-11 12:46:38 +0200

    First Commit

---
+++ Exercitiul 1/script.js
@@ -0,0 +1,31 @@
+// Problema 1 
+
+const ex1 = 2 + 3 * 4 - 5;
+
+console.log(ex1);
+
+// Problema 2
+
+const ex2 = 4 * (2 + 6 / 3);
+
+console.log(ex2);
+
+// Problema 3
+
+const ex3 = 2 * 3 ** 2 + 2 ** 5;
+
+console.log(ex3);
+
+
+// Problema 4
+
+const ex4 = [5 * (3 + 2 / 2)] + 3;
+
+console.log(ex4);
+
+// Problema 5
+
+
+const ex5 = ([2 ** 5 - 5 ** 2 + 3 * (5 - 4 / 2)]) / 2 ** 5 + 5 ** 2;
+
+console.log(ex5);
---
+++ Exercitiul 2/Problema 2.js
@@ -0,0 +1,14 @@
+// Problema 2
+
+
+let x = 5;
+let y = 8;
+
+// Ex 1 
+
+ex1 = 2 * x - 5 * y;
+
+console.log(ex1);
+
+ex2 = 3 * x + 9 * y;
+console.log(ex2);
